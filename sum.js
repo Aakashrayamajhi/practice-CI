f9c2a8bc0d285706d64d5024bfbc866378f9ca10
Aakashrayamajhi <aakashrayamajhi03@gmail.com>
2026-05-21 08:25:00 +0545

    refactor sum.js

--- sum.js
+++ sum.js
@@ -1,5 +1,5 @@
 const sum = (a,b)=>{
-    return a+b
+    return a+b;
 }
 
-sum(10,10) // ok yeslai chai aaba correct gare ra check garam .
+console.log(sum(10, 10));// ok yeslai chai aaba correct gare ra check garam .
