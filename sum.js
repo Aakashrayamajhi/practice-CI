610a9d4c02142ef00c62d80a6a96604a97ec86b1
Aakashrayamajhi <aakashrayamajhi03@gmail.com>
2026-05-21 11:29:03 +0545

    next lint error to check

--- sum.js
+++ sum.js
@@ -7,5 +7,5 @@ console.log(sum(10, 10));// ok yeslai chai aaba correct gare ra check garam .
 
 const sum2 = (a,b)=>{
     return a+b;
-}dsf
+} // dsf removed then lets see another errort toidentify
 sum2ddd  //lint check using github action
