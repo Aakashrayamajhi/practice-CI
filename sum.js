64cab4c7a2b47e69c6f465bbf0f67443c86982a0
Aakashrayamajhi <aakashrayamajhi03@gmail.com>
2026-05-21 11:11:05 +0545

    test lint.yml

--- sum.js
+++ sum.js
@@ -3,3 +3,9 @@ const sum = (a,b)=>{
 }
 
 console.log(sum(10, 10));// ok yeslai chai aaba correct gare ra check garam .
+
+
+const sum2 = (a,b)=>{
+    return a+b;
+}
+sum2ddd  //lint check using github action
